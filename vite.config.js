59e40e30029db4ca71fdd2a76afae24d27becc36
Brandonlarsencode <brandon.satan.larsen@gmail.com>
2024-04-11 00:18:22 +0200

    commit

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [react()],
   base: "/3D-portfolio",
   build: {
     rollupOptions: {
-      external: ['react-dom/client']
+      external: ['react-dom']
     }
   }
 })
