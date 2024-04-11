68900ec38de5056ab7695a23f571f46730a6148f
Brandonlarsencode <brandon.satan.larsen@gmail.com>
2024-04-11 22:42:44 +0200

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,8 @@
 import React from "react";
-import { createRoot } from 'react-dom/client';
+import ReactDOM from "react-dom";
+
 
 import App from "./App";
 import "./index.css";
 
-createRoot(document.getElementById("root")).render(
-  <App />
-);
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
